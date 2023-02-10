ed8df1c9542fae32d0ce0f71bc7836ccee5005a7
usama-365 <usama-365@protonmail.com>
2023-02-10 22:15:52 +0500

    Added basic routing structure

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require("node:http");
+const url = require("node:url");
 
 const PORT = 8000;
 const HOST = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
-    res.end("Server is working");
+    const path = req.url;
+    switch (path) {
+        case "/overview":
+        case "/":
+            res.end("This is the OVERVIEW");
+            break;
+        case "/product":
+            res.end("This is the PRODUCT");
+            break;
+        default:
+            res.writeHead(404, {
+                "Content-type": "text/html"
+            });
+            res.end("<h1>Page not found</h1>");
+    }
 });
 
 server.listen(PORT, HOST, () => {
