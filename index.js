6adc4adcb1ec6f619b69e04007b1151d83401da7
usama-365 <usama-365@protonmail.com>
2023-02-11 09:32:10 +0500

    Created a simple API endpoint

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("node:http");
+const fs = require("node:fs");
 const url = require("node:url");
 
 const PORT = 8000;
 const HOST = "127.0.0.1";
+const DATA_FILE = `${__dirname}/data/data.json`;
+
+const data = fs.readFileSync(DATA_FILE, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const path = req.url;
@@ -13,6 +18,12 @@ const server = http.createServer((req, res) => {
             break;
         case "/product":
             res.end("This is the PRODUCT");
+            break;
+        case '/api':
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            })
+            res.end(data);
             break;
         default:
             res.writeHead(404, {
